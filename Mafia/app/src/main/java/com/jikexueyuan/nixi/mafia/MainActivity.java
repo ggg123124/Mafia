99d7fcbe2bda7d47461136c65a6da27cdf5b9b90
ggg123124 <842803916@qq.com>
2016-04-23 02:33:07 +0800

    加入了新的即时通讯组件和一些h不可预料的bug

--- Mafia/app/src/main/java/com/jikexueyuan/nixi/mafia/MainActivity.java
+++ Mafia/app/src/main/java/com/jikexueyuan/nixi/mafia/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import cn.bmob.newim.BmobIM;
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.listener.SaveListener;
 
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //NewIM初始化
+        BmobIM.init(this);
+        //注册消息接收器
+        //// TODO: 2016/4/23 这个地方注册的可能有问题，如果在之后的开发中出现消息接收器异常需要检查这里
+        BmobIM.registerDefaultMessageHandler(new MessageHandler());
     }
 }
